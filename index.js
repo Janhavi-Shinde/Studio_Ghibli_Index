12a73f5f2c877b7405c310742dbb18e8d82f333a
janhavi <janashinde394@gmail.com>
2022-03-10 01:14:38 +1100

    fixed 'file error' bug - all 3 files now successgully linked

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-fetch('https://ghibliapi.herokuapp.com')
+// function getData() {
+//     const response = await fetch('https://ghibliapi.herokuapp.com/films');
+//     const data = await response.json();
+//   }
+
+//   var data = JSON.parse(this.response)
+
+// data.forEach(movie => {
+//   // Log each movie's title
+//   console.log(movie.title)
+// })
+
+
+// const getMovieData = () => {
+// 'https://ghibliapi.herokuapp.com/films'};
+// fetch('https://ghibliapi.herokuapp.com/films') 
+// .then (response => response.json())
+// .then (data => console.log(data))
+
+
+// const getAPI = async () => {
+//     const response = await fetch('https://ghibliapi.herokuapp.com/films');
+//     const data = await response.json();
+//     return data
+// };
+// getAPI()
+// .then(data => console.log('resolved:', data));
+
+fetch('https://ghibliapi.herokuapp.com/films') 
+.then (response => response.json())
+.then (json => console.log(json));
+
