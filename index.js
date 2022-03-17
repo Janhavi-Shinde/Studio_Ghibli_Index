35da729bbe323c28e949c5e67dee10b70d5062ad
janhavi <janashinde394@gmail.com>
2022-03-17 15:59:45 +1100

    like button working

--- index.js
+++ index.js
@@ -22,92 +22,94 @@ async function fetchAPIdata() {
     try {
         const response = await fetch("https://ghibliapi.herokuapp.com/films")
         const data = await response.json();
-        state = data 
-        
+        state = data
+
         boxedFilms();
         searchResultFilms(state);
-        
+
         // bannerArrayFunction(state);
-                                // ?unnecessary// 
-                                    	        // retrievingBanners(state);
-                                // ?unnecessary// 
+        // ?unnecessary// 
+        // retrievingBanners(state);
+        // ?unnecessary// 
     } catch (error) {
-        console.error(error)  
+        console.error(error)
     }
 }
-fetchAPIdata();  
+fetchAPIdata();
 
 
 
 
 // FILMS + THEIR DESCRIPTIONS
-function boxedFilms () { 
+function boxedFilms() {
     state.forEach(movie => {
-    
-    const box = document.createElement('div') 
-    box.setAttribute('class', 'box' ) 
-                    
-    const h2 = document.createElement('h2')
-    h2.innerHTML = movie.title
+
+        const box = document.createElement('div')
+        box.setAttribute('class', 'box')
+
+        const h2 = document.createElement('h2')
+        h2.innerHTML = movie.title
 
 
-    const information = document.createElement('p')
-    information.innerHTML = `${movie.description}`
-    
+        const information = document.createElement('p')
+        information.innerHTML = `${movie.description}`
 
-    box.appendChild(h2)
-    box.appendChild(information)
-    films.appendChild(box)
-}) } 
+
+        box.appendChild(h2)
+        box.appendChild(information)
+        films.appendChild(box)
+    })
+}
 
 // SEARCH BAR: 
 
-            // EVENT LISTENER
+// EVENT LISTENER
 searchBar.addEventListener("keydown", (e) => {
     // console.log(e.target.value);
     const searchString = e.target.value.toLowerCase();
-    const filteredTitles = state.filter((state) => { 
-        return (state.title.toLowerCase().includes(searchString) || 
-                state.director.toLowerCase().includes(searchString))
-                
+    const filteredTitles = state.filter((state) => {
+        return (state.title.toLowerCase().includes(searchString) ||
+            state.director.toLowerCase().includes(searchString))
+
     });
     // console.log(filteredTitles);
     searchResultFilms(filteredTitles);
 });
 
 
-            // DISPLAY OF FILMS RETURNED FROM THE SEARCH
+// DISPLAY OF FILMS RETURNED FROM THE SEARCH
 const searchResultFilms = (films) => {
     const htmlString = films
         .map((films) => {
             return ` 
             <li class ="films-list">
                 	<h2>${films.title}</h2>
-                    <h4>Directed by ${films.director}</h4> 
+                    <h4>Directed by: <br> ${films.director}</h4> 
                     <img class="filmPoster" src="${films.image}" />
                     <button class="showModalBtn" onclick="onClick(event)" value="${films.description}"> Description </button>       
                     <div id="containerForHeartIcon" >
-                        <i id="heartIcon"  class="fa-regular fa-heart"> </i>
+                        <i onclick="toggler(event)" id="heartIcon" class="fa-regular fa-heart"> </i>
                     </div>                   
             </li>`;
         })
-        .join(''); 
-       filmList.innerHTML = htmlString;
+        .join('');
+    filmList.innerHTML = htmlString;
 };
 
 
-const containerForHeartIcon = document.getElementById('containerForHeartIcon');
+const containerForHeartIcon = document.querySelector('#containerForHeartIcon');
 const heartIcon = document.getElementById('heartIcon');
 
-// function toggle() {
-//         if (heartIcon.classList.contains("fa-regular fa-heart")) {
-//         heartIcon.classList.remove('fa-regular fa-heart');
-//         heartIcon.classList.add('fa-solid fa-heart');
-//     } else {
-//         heartIcon.classList.remove('fa-solid fa-heart');
-//         heartIcon.classList.add('fa-regular fa-heart');
-//     } 
-// }
+function toggler(event) {
+    const target = event.target;
+    console.log(target)
+    if (target.classList.contains("fa-regular")) {
+        target.classList.replace('fa-regular', 'fa-solid');
+
+    } else {
+        target.classList.replace('fa-solid', 'fa-regular');
+    }
+}
 
 // function toggle() { 
 //     containerForHeartIcon.style.backgroundColor = '#df3e3e'
@@ -118,13 +120,11 @@ const heartIcon = document.getElementById('heartIcon');
 
 
 
-
-
 // MODAL
-function onClick (event) {
+function onClick(event) {
     const target = event.target;
     overlay[0].classList.toggle('hidden');
-    descriptionParagraph[0].innerHTML = target.value; 
+    descriptionParagraph[0].innerHTML = target.value;
 }
 
 
@@ -138,17 +138,16 @@ closeModalBtn[0].addEventListener("click", (e) => {
 
 
 
-backToTop.addEventListener("click", ()=> { 
+backToTop.addEventListener("click", () => {
     document.documentElement.scrollTop = 0;
 })
+
+
+
 
 containerForHeartIcon.addEventListener("click", () => {
     containerForHeartIcon.style.backgroundColor = '#df3e3e'
 })
-
-
-
-
 
 
 
@@ -181,41 +180,40 @@ containerForHeartIcon.addEventListener("click", () => {
 const bannerArrayFunction = (picture) => {
     const listOfBannerImages = picture
         .map((picture) => {
-            return`
+            return `
             <li class="banner-list">
                 <img src="${picture.movie_banner}" alt="${picture.title}"/>     
             </li>`;
-    })
-        .join(''); 
-        bannerList.innerHTML = listOfBannerImages;
+        })
+        .join('');
+    bannerList.innerHTML = listOfBannerImages;
     //consider adding a after/before pseudoElement to have the name of the respective movie, showing up in the corner?//
-            console.log(listOfBannerImages)
-        }; //listOfBannerImages contains the (joined)array of banner images in the list tags
+    console.log(listOfBannerImages)
+}; //listOfBannerImages contains the (joined)array of banner images in the list tags
 
 
 
 
 
-                        // ?unnecessary// 
-                        const retrievingBanners = (picture) => {
-                                const filmBanners = picture.map((picture) =>{
-                                        return picture.movie_banner;
-                                })
-                                // console.log(filmBanners);
-                        }
-                        // ?unnecessary// 
-
-
-
-    
+// ?unnecessary// 
+const retrievingBanners = (picture) => {
+    const filmBanners = picture.map((picture) => {
+        return picture.movie_banner;
+    })
+    // console.log(filmBanners);
+}
+                        // ?unnecessary//
 
 
 
 
 
 
-        
-    
 
 
-  
+
+
+
+
+
+
---
+++ liker/liker.js
@@ -0,0 +1,30 @@
+const heartIcon = document.getElementById('heartcIcon');
+const likerContainer = document.getElementById('likerContainer');
+
+
+document.addEventListener('DOMContentLoaded', ()=> { 
+    console.log('dom has loaded');
+})
+
+
+
+
+
+function liker (){ 
+    if(heartIcon.className ==='fa-regular fa-heart') {
+        heartIcon.classList.replace('fa-regular', 'fa-solid')
+    } else { 
+        heartIcon.classList.replace('fa-solid', 'fa-regular')
+    }
+}
+
+
+
+// function liker (){
+//     if(likerContainer.style.backgroundColor === '#fff'){ 
+//         likerContainer.style.backgroundColor === '#ff0000'  
+//      }
+//         else if (likerContainer.style.backgroundColor === '#ff0000'){
+//             likerContainer.style.backgroundColor === '#fff' 
+//         }
+// }
